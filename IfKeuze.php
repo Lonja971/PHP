7e18e299c32ff50e910317a874755106dd315f7c
MrStinger <leonidalekseev59@gmail.com>
2023-09-21 09:27:47 +0200

    IfKeuze 0.2

--- IfKeuze.php
+++ IfKeuze.php
@@ -2,16 +2,23 @@
 
 $naam = readline("Geef jouw naam: ");
 $leeftijd = readline("Geef jouw leeftijd: ");
-echo "Hallo $naam! \n";
 
-if ($leeftijd < 22) {
-   echo "Jij bent jonger dan 22 jaar.\n";
-}
-if ($leeftijd > 22) {
-   echo "Jij bent ouder dan 22 jaar \n";
-}
-if ($leeftijd == 22) {
-   echo "Jij bent precies 22 jaar. \n";
+echo "\nHallo $naam! \n";
+
+if (ctype_alpha($leeftijd)) {
+   echo "Het is geen getal :(\n";
+}elseif (is_numeric($leeftijd)) {
+   if ($leeftijd < 22) {
+      echo "Jij bent jonger dan 22 jaar.\n";
+   }
+   if ($leeftijd > 22) {
+      echo "Jij bent ouder dan 22 jaar \n";
+   }
+   if ($leeftijd == 22) {
+      echo "Jij bent precies 22 jaar. \n";
+   }
+}else{
+   echo "ERROR";
 }
 
 ?>
