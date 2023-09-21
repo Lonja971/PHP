113d1ba3d29dcf97a80614a017fee55d2d2ff168
MrStinger <leonidalekseev59@gmail.com>
2023-09-21 13:08:14 +0200

    modified:   Quiz.php

--- Quiz.php
+++ Quiz.php
@@ -11,27 +11,27 @@ check($random, $keer);
 
 function check($random, $keer) {
    $antwoord = readline("Voer de waarde: ");
-   if (ctype_alpha($antwoord)) {
+   if ($antwoord == "stop"){
+      exit;
+   }elseif (ctype_alpha($antwoord)) {
       echo "Weet jij wat een getal is? Probeer het nog eens.\n";
       check($random, $keer);
    }elseif (is_numeric($antwoord)) {
-      if ($antwoord == "stop"){
-         exit;
       }elseif ($random == $antwoord){
          echo "Je hebt het geraden! Het was ".$random."!\n";
          echo "Je raadt het al in ".$keer." stappen!";
       }elseif ($antwoord > 100) {
-         echo "Dit aantal is meer dan 100 -_-";
+         echo "Dit aantal is meer dan 100 -_-\n";
          check($random, $keer);
       }elseif ($antwoord < 1) {
-         echo "Dit aantal is kleiner dan 0 -_-";
+         echo "Dit aantal is kleiner dan 0 -_-\n";
          check($random, $keer);
       }elseif ($random < $antwoord){
          echo "No(; Maar het geraden getal is kleiner dan ".$antwoord."\n";
          $keer++;
          check($random, $keer);
       }elseif ($random > $antwoord){
-         echo "No(; Maar het geraden getal is groter dan ".$antwoord."\n";
+         echo "Nee, groter dan ".$antwoord."🤓\n";
          $keer++;
          check($random, $keer);
       }
