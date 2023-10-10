938541767b4e2c34638ff191dabd10aaea993f1b
MrStinger <leonidalekseev59@gmail.com>
2023-10-10 11:55:54 +0200

    new file:   Breuken.php

---
+++ Breuken.php
@@ -0,0 +1,53 @@
+<?php
+
+echo "\nHallo! Dit programma helpt je getallen te verdelen!\n";
+$voorwaarde = true;
+
+while ($voorwaarde == true){
+   $deeltalAudit = true;
+   while ($deeltalAudit == true){
+      $deeltal = readline("Voer een deeltal in: ");
+      if (!is_numeric($deeltal)){
+         echo "Deeltal is geen geldig nummer. Probeer het opnieuw.\n";
+      }else{
+         $deeltalAudit = false;
+      }
+   }
+
+   $delerAudit = true;
+   while ($delerAudit == true){
+      $deler = readline("Voer een deler in: ");
+      if (!is_numeric($deler)){
+         echo "Deeltal is geen geldig nummer. Probeer het opnieuw.\n";
+      }else{
+         $delerAudit = false;
+      }
+   }
+   
+   $resultaat = $deeltal/$deler;
+   
+   $fullRes = intval($resultaat);
+   $rest = $deeltal % $deler;
+   
+   if ($rest == 0){
+      echo "\n".$deeltal." / ".$deler." = ".$fullRes."\n";
+   }else{
+      echo "\n".$deeltal." / ".$deler." = ".$fullRes." (".$rest."/".$deler.")\n";
+   }
+   $vervolgcontrole = true;
+   while ($vervolgcontrole == true){
+      $keuze = readline("Wil je doorgaan? Ja/Nee: ");
+      if (strtolower($keuze) == "nee"){
+         echo "\nBedankt voor het gebruik :)";
+         $vervolgcontrole = false;
+         $voorwaarde = false;
+      }elseif (strtolower($keuze) == "ja"){
+         echo "\n";
+         $vervolgcontrole = false;
+      }else{
+         echo "Ik begrijp jou niet :(\n";
+      }
+   }
+}
+
+?>
