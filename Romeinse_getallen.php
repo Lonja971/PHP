ae667598384ff373765fc9cc0601f0c3498d0268
MrStinger <leonidalekseev59@gmail.com>
2023-11-08 13:49:27 +0100

    modified:   Romeinse_getallen.php

--- Romeinse_getallen.php
+++ Romeinse_getallen.php
@@ -10,9 +10,13 @@ function startProgramm() {
         if (preg_match('/^[xvimlcd]+$/i', $userNumber)) {
             $arabicNumber = convertRomanToArabic($userNumber);
             $goodNumber = false;
-        }elseif (is_numeric($userNumber)) {
-            convertArabicToRoman($userNumber); 
-            $goodNumber = false;
+        } elseif (is_numeric($userNumber)) {
+            if ($userNumber > 4999){
+                echo "Het aantal is te groot.\n";
+            }else{
+                convertArabicToRoman($userNumber);
+                $goodNumber = false;
+            }
         } else {
             echo "Het spijt me, ik begrijp het niet. Herhaal dit alstublieft.\n";
         }
@@ -34,47 +38,46 @@ function startProgramm() {
     }
 }
 
-
 function convertRomanToArabic($input) {
-   $romanToArabic = [
-       'CM' => 900,
-       'CD' => 400,
-       'XC' => 90,
-       'XL' => 40,
-       'IX' => 9,
-       'IV' => 4,
-       'M' => 1000,
-       'D' => 500,
-       'C' => 100,
-       'L' => 50,
-       'X' => 10,
-       'V' => 5,
-       'I' => 1,
-   ];
+    $romanToArabic = [
+        'CM' => 900,
+        'CD' => 400,
+        'XC' => 90,
+        'XL' => 40,
+        'IX' => 9,
+        'IV' => 4,
+        'M' => 1000,
+        'D' => 500,
+        'C' => 100,
+        'L' => 50,
+        'X' => 10,
+        'V' => 5,
+        'I' => 1,
+    ];
 
-   $input = strtoupper($input);
-   $example = $input;
+    $input = strtoupper($input);
+    $example = $input;
 
-   $output = 0;
+    $output = 0;
 
-   foreach ($romanToArabic as $roman => $arabic) {
-       while (strpos($input, $roman) === 0) {
-           $output += $arabic;
-           $input = substr_replace($input, '', 0, strlen($roman));
-       }
-   }
+    foreach ($romanToArabic as $roman => $arabic) {
+        while (strpos($input, $roman) === 0) {
+            $output += $arabic;
+            $input = substr_replace($input, '', 0, strlen($roman));
+        }
+    }
 
-   echo "Romeins cijfer $example omgezet naar Arabisch cijfer: $output\n";
+    echo "Romeins cijfer $example omgezet naar Arabisch cijfer: $output\n";
 
-   return $output;
+    return $output;
 }
-
 
 function convertArabicToRoman($input) {
     $arabicNumber = intval($input);
     $romanNumber = '';
-    if ($arabicNumber >= 1 && $arabicNumber <= 3999) {
+    if ($arabicNumber >= 1) {
         $arabicToRoman = [
+            4000 => 'MMMM',
             1000 => 'M',
             900 => 'CM',
             500 => 'D',
@@ -102,5 +105,4 @@ function convertArabicToRoman($input) {
         echo "Het Arabische cijfer $input wordt omgezet in een Romeins cijfer: $romanNumber\n";
     }
 }
-
 ?>
